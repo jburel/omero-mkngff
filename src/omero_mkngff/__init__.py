36242c8a86bed1eedb3ba5d4c3f1fc7837d1e864
William Moore <w.moore@dundee.ac.uk>
2023-08-21 13:24:51 +0100

    Revert "temporary register chunks"
    This reverts commit 9a001a3b0968d35bfbf84040b37583ff2cab3c52.

--- src/omero_mkngff/__init__.py
+++ src/omero_mkngff/__init__.py
@@ -244,8 +244,12 @@ class MkngffControl(BaseControl):
             if not p.is_dir():
                 yield (p.parent, p.name, "application/octet-stream")
             else:
-                yield (p.parent, p.name, "Directory")
-                yield from self.walk(p)
+                if (p / ".zarray").exists() or (p / ".zgroup").exists():
+                    yield (p.parent, p.name, "Directory")
+                    yield from self.walk(p)
+                else:
+                    # Chunk directory
+                    continue
 
     def get_uuid(self, args: Namespace) -> str:
         from omero.grid import ManagedRepositoryPrx as MRepo
